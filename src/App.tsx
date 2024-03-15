be06b28f2f745552dc9c235dbd6334094a1ed8af
Lucas Danilo <lukastecno@gmail.com>
2024-03-15 15:29:41 -0300

    Page about

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Computers from "./routes/Products/Computers";
 import Products from "./routes/Products";
 import Electronics from "./routes/Products/Electronics";
 import Book from "./routes/Products/Book";
+import About from "./routes/Products/About";
 
 export default function App() {
 
@@ -17,6 +18,7 @@ export default function App() {
           <Route path="/computers" element={<Computers />} />
           <Route path="/electronics" element={<Electronics />} />
           <Route path="/book" element={<Book />} />
+          <Route path="/about" element={<About />} />
         </Route>
       </Routes>
     </BrowserRouter >
---
+++ src/routes/Products/About/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+export default function About({ content_page }: any) {
+
+    return (
+
+        <main>
+            <div className="about">
+                <h1>{content_page = "Sobre nós"}</h1>
+            </div>
+        </main>
+    );
+}
