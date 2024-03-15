6f53b7cff5da2d946fe111d7a02b13ba9cb6064c
Lucas Danilo <lukastecno@gmail.com>
2024-03-15 01:07:45 -0300

    Page computers

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import HomeBody from "./routes/Home/HomeBody";
 import Products from "./routes/Products";
+import Computers from "./routes/Products/Computers";
 
 export default function App() {
 
@@ -11,7 +12,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} >
           <Route index element={<HomeBody />} />
-          <Route path="/products" element={<Products/>} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/computers" element={<Computers />}>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/routes/Products/Computers/index.tsx
@@ -0,0 +1,20 @@
+import Products from '..';
+import './styles.css';
+
+export default function Computers() {
+
+    return (
+        <>
+
+            <Products />
+            <div className="list-computer">
+                <ul className="list-products">
+                    <li><a href="#">Computador 8GB</a> </li>
+                    <li><a href="#">Computador 16GB</a> </li>
+                    <li><a href="#">Computador 32GB</a> </li>
+                </ul>
+            </div>
+        </>
+
+    );
+}
