9edef364ac7270d8b5c2908253584d08d473f026
Lucas Danilo <lukastecno@gmail.com>
2024-03-14 22:38:28 -0300

    Outlet, Homebody and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import HomeBody from "./routes/Home/HomeBody";
 
 export default function App() {
 
   return (
-    <Header />
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} >
+          <Route index element={<HomeBody />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/routes/Home/HomeBody/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+export default function HomeBody() {
+
+    return (
+
+        <main className="home-page-container">
+            <div className="title">
+                <h1>Bem-vindos!</h1>
+            </div>
+        </main>
+
+    );
+}
